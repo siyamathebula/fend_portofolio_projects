e468c2686fe5f971f90c252b5695751e907c211b
Siya Mathebula <siya@differentlife.co.za>
2019-08-01 10:58:34 +0200

    Add final project version to github

---
+++ restaurant_reviews_app/README.md
@@ -0,0 +1,20 @@
+# Restaurant Reviews App Stage 1
+
+## Restaurant Review Site
+The site is accessible and responsive on all devices and uses a simple file caching mechanism for ease of use
+
+## Leaflet.js and Mapbox:
+
+This folder uses [leafletjs](https://leafletjs.com/) with [Mapbox](https://www.mapbox.com/). The required mapbox token is hardcoded within the relevant files
+
+
+## Launching app locally
+1) Download the zip folder
+2) cd into the extracted zip folder
+3) If you have python 2.x run `python -m SimpleHTTPServer 7999`
+4) If you have python 3.x run `python3 -m http.server 7999` (you can download it here https://www.python.org/downloads/)
+5) With the server running visit `http://localhost:7999`
+6) If the port is already in use, you can access the js/dbhelper.js and change it on line 11
+7) Change the port from dbhelper.js if 8777 is already in use
+
+
---
+++ restaurant_reviews_app/sw.js
@@ -0,0 +1,65 @@
+let staticCacheName = 'restaurant-static-v1';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll([
+                '/index.html',
+                '/restaurant.html',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/js/serviceWorker.js',
+                '/css/styles.css',
+                '/data/restaurants.json',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/restaurant.html?id=1',
+                '/restaurant.html?id=2',
+                '/restaurant.html?id=3',
+                '/restaurant.html?id=4',
+                '/restaurant.html?id=5',
+                '/restaurant.html?id=6',
+                '/restaurant.html?id=7',
+                '/restaurant.html?id=8',
+                '/restaurant.html?id=9',
+                '/restaurant.html?id=10'
+            ]);
+            
+        })
+
+    );
+    
+});
+self.addEventListener('activate', function (event) {
+    
+    event.waitUntil(
+        caches.keys()
+            .then(function (cacheNames) {
+                return Promise.all(
+                    cacheNames.filter(function (cacheName) {
+                        return cacheName.startsWith('restaurant-') &&
+                            cacheName != staticCacheName;
+                    }).map(function (cacheName) {
+                        return caches.delete(cacheName);
+                    })
+                );
+            })
+    );
+})
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+    );
+});
